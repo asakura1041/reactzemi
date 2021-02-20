69c5aebde86edabbd2fb5208b6449d398fc8dd3e
asakura1041 <toshiteru.asakura@futurelink.co.jp>
2021-02-20 23:51:39 +0900

    test hooks

--- src/Article.jsx
+++ src/Article.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import LikeButton from './LikeButton'
+import React, {useState} from 'react';
+import LikeButton from './LikeButton';
 
 const Article = (props) => {
+  const [isPublished, togglePublished] = useState(false);
+
   return (
     <div>
       <h2>{props.title}</h2>
-      <input type="checkbox" checked={props.isPublished} onClick={() => props.toggle()}/>
-      <LikeButton count={props.count} />
+      <input type="checkbox" checked={isPublished} onClick={() => togglePublished(!isPublished)}/>
+      {/* <LikeButton count={props.count} /> */}
     </div>
   )
 }
--- src/Blog.jsx
+++ src/Blog.jsx
@@ -3,57 +3,39 @@ import Article from './Article';
 import * as FooBar from './components/Foobar';
 import Hoge from './components/Hoge';
 
-class Blog extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isPublished: false,
-      count: 0
-    }
-  }
+const Blog = () => {
+  // componentDidMount() {
+  //   document.getElementById('counter').addEventListener('click', this.countUp)
+  // }
 
-  componentDidMount() {
-    document.getElementById('counter').addEventListener('click', this.countUp)
-  }
+  // componentDidUpdate() {
+  //   console.log(this.state.count)
+  //   if (this.state.count >= 10) {
+  //     this.setState({count: 0})
+  //   }
+  // }
 
-  componentDidUpdate() {
-    console.log(this.state.count)
-    if (this.state.count >= 10) {
-      this.setState({count: 0})
-    }
-  }
+  // componentWillUnmount() {
+  //   document.getElementById('counter').removeEventListener('click', this.countUp)
+  // }
 
-  componentWillUnmount() {
-    document.getElementById('counter').removeEventListener('click', this.countUp)
-  }
+  // countUp = () => {
+  //   this.setState({
+  //     count: this.state.count + 1
+  //   })
+  // }
 
-  togglePublished = () => {
-    this.setState({
-      isPublished: !this.state.isPublished
-    })
-  }
-
-  countUp = () => {
-    this.setState({
-      count: this.state.count + 1
-    })
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <Article 
-          title={"Reactの使い方"} 
-          isPublished={this.state.isPublished} 
-          toggle={() => this.togglePublished()}
-          count={this.state.count}
-        />
-        <FooBar.Foo />
-        <FooBar.Bar />
-        <Hoge />
-      </React.Fragment>
-    )
-  }
+  return (
+    <React.Fragment>
+      <Article 
+        title={"Reactの使い方"} 
+        // count={this.state.count}
+      />
+      <FooBar.Foo />
+      <FooBar.Bar />
+      <Hoge />
+    </React.Fragment>
+  )
 }
 
 export default Blog
